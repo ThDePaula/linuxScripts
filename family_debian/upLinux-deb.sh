0e2e536ac04e3748f37b04190e5bae93d6c06133
ThDePaula <titidepaula22@gmail.com>
2022-11-22 16:58:55 -0300

    Reformulação no Script de Atualização

--- family_debian/upLinux-deb.sh
+++ family_debian/upLinux-deb.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt install -y
-sudo apt autoremove -y
-sudo apt autoclean -y
-flatpak update -y
+GREEN_BOLD='\033[1;32m'
+YELLOW_BOLD='\033[1;33m'
+BLUE_BOLD='\033[1;34m'
+NO_COLOR='\e[0m'
+
+apt_update(){
+    echo -e "${BLUE_BOLD}[INFO] - System Update ${NO_COLOR}"
+    
+    sudo apt update -y
+    sudo apt upgrade -y
+    flatpak update -y
+}
+
+apt_remove(){
+    echo -e "${YELLOW_BOLD}[INFO] - Remove Legacy Packages ${NO_COLOR}"
+
+    sudo apt autoremove -y
+    sudo apt autoclean -y
+}
+
+apt_update
+apt_remove
+
+echo -e "${GREEN_BOLD}[INFO] - Update Finished!!!${NO_COLOR}"
--- family_redhat/upLinux-redhat.sh
+++ family_redhat/upLinux-redhat.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-sudo dnf update -y
-sudo dnf upgrade -y
-sudo dnf autoremove -y
-sudo dnf clean all -y
-flatpak update -y
+GREEN_BOLD='\033[1;32m'
+YELLOW_BOLD='\033[1;33m'
+BLUE_BOLD='\033[1;34m'
+NO_COLOR='\e[0m'
+
+dnf_update(){
+    echo -e "${BLUE_BOLD}[INFO] - System Update ${NO_COLOR}"
+
+    sudo dnf update -y
+    sudo dnf upgrade -y
+    flatpak update -y
+}
+
+dnf_remove(){
+    echo -e "${YELLOW_BOLD}[INFO] - Remove Legacy Packages ${NO_COLOR}"
+    
+    sudo dnf autoremove -y
+    sudo dnf clean all -y
+}
+
+dnf_update
+dnf_remove
+
+echo -e "${GREEN_BOLD}[INFO] - Update Finished!!!${NO_COLOR}"
